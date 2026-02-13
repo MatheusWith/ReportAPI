cf2c8d608f8bc9ce6f5c81bbaa68c8ee7b68fd72
MatheusWith <matheuswithsilva@gmail.com>
2026-02-13 11:37:57 -0300

    remove break line

--- src/app/controllers/controller.py
+++ src/app/controllers/controller.py
@@ -4,6 +4,4 @@ from src.app.controllers.v1.controller import controller as controller_fastapi
 
 controller = APIRouter(prefix="/api")
 
-controller.include_router(
-    controller_fastapi
-)
+controller.include_router(controller_fastapi)
