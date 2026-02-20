b3da0cb6068af81203bdf4413dcc5340704aaf3f
MatheusWith <matheuswithsilva@gmail.com>
2026-02-20 14:18:39 -0300

    fix dba url

--- src/app/service/dependencies.py
+++ src/app/service/dependencies.py
@@ -1,6 +1,18 @@
+import logging
 from pathlib import Path
 
+from src.app.core.config import settings
 
-def read_sql_query(sql_path: str) -> str:
+base_file:str = settings.BASE_FILE
+
+LOGGER = logging.getLogger(__name__)
+
+def read_sql_query(sql_path: str) -> str | None:
     """Read SQL file as string."""
-    return Path(sql_path).read_text()
+    sql = Path(base_file + sql_path)
+    if sql.exists():
+        return sql.read_text()
+    else:
+        LOGGER.error("Files not found")
+        return None
+
